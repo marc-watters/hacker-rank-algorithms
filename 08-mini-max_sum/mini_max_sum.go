e717174c225241d7c5fefe9fbbbfdd190f2db6f4
Marc Watters <marc.watters@proton.me>
2025-01-31 00:46:12 -0500

    08 - Mini-Max Sum
    Given five positive integers, find the minimum and maximum values that
    can be calculated by summing exactly four of the five integers. Then
    print the respective minimum and maximum values as a single line of two
    space-separated long integers.
    
    Example
    
    arr = [1, 3, 5, 7, 9]
    
    The minimum sum is 1+3+5+7=16 and the maximum sum is 3+5+7+9=24. The
    function prints 16 24

---
+++ 08-mini-max_sum/mini_max_sum.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func miniMaxSum(arr []int32) {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+
+	var minSum, maxSum uint64
+	for _, n := range arr[:4] {
+		minSum += uint64(n)
+	}
+
+	for _, n := range arr[1:] {
+		maxSum += uint64(n)
+	}
+
+	fmt.Print(minSum, maxSum)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < 5; i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
